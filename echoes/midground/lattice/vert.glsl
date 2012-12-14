1b8e7b42bbdf55e594d0548b2ed7bed2d99785cc
Alex Wayne <alex@beautifulpixel.com>
2012-12-13 21:58:49 -0800

    Lattice dances around a bit more

--- echoes/midground/lattice/vert.glsl
+++ echoes/midground/lattice/vert.glsl
@@ -6,20 +6,6 @@ uniform float skew;
 uniform float twistDir;
 
 void main() {
-  // Pass to fragment shader
-  vec3 transPos = (modelViewMatrix * vec4(position, 1.0)).xyz;
   uvCoord = uv;
-
-  vec3 origin = vec3(0.0, -40.0, 0.0);
-
-  float depth = uv.y;
-  float swirl = pow(depth, skew) * twist * twistDir;
-
-  vec3 twisted = vec3(
-    (transPos.x * cos(swirl) - transPos.y * sin(swirl)) * (1.0 - depth),
-    (transPos.x * sin(swirl) + transPos.y * cos(swirl)) * (1.0 - depth),
-    transPos.z + twistDir
-  );
-
-  gl_Position =  projectionMatrix * vec4(twisted, 1.0);
+  gl_Position =  projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
 }
