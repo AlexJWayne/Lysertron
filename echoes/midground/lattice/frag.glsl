ce6c93b8ad12b7379760d45a7c8176b794665448
Alex Wayne <alex@beautifulpixel.com>
2012-12-13 22:06:30 -0800

    lattice transitions

--- echoes/midground/lattice/frag.glsl
+++ echoes/midground/lattice/frag.glsl
@@ -7,7 +7,7 @@ void main() {
   float alpha = 1.0 - pow(1.0 - uvCoord.y, 7.0);
   vec3 depthTint = color + vec3(smoothstep(0.0, 1.0, uvCoord.y));
 
-  float shadow = abs(uvCoord.x * 2.0 - 1.0) * 0.2;
+  float shadow = abs(uvCoord.x * 2.0 - 1.0) * 0.15;
   
   gl_FragColor = vec4(depthTint - vec3(shadow), alpha);
 }
