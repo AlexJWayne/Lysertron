8992d87c5d7ce89f0d3243ec6cf4acf6788fd8c9
Alex Wayne <alex@beautifulpixel.com>
2012-12-15 18:09:18 -0800

    Bulge middle of lattice

--- echoes/midground/lattice/vert.glsl
+++ echoes/midground/lattice/vert.glsl
@@ -5,7 +5,15 @@ uniform float twist;
 uniform float skew;
 uniform float twistDir;
 
+uniform vec3 bulge;
+
 void main() {
+
   uvCoord = uv;
-  gl_Position =  projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
+  vec4 vPos = modelViewMatrix * vec4(position, 1.0);
+  vPos.xyz += sin(uv.y * 3.14159) * bulge;
+  
+  vPos.xy *= 0.9 + abs(uv.x * 2.0 - 1.0) * 0.1;
+
+  gl_Position =  projectionMatrix * vPos;
 }
