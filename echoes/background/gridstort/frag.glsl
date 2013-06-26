e62ac650eb8d0cf339b51ea1b89c19c95a964983
Alex Wayne <alex@beautifulpixel.com>
2013-06-26 03:46:20 -0700

    gridstort now has multiple ripples.

--- echoes/background/gridstort/frag.glsl
+++ echoes/background/gridstort/frag.glsl
@@ -1,31 +1,50 @@
 varying vec2 uvCoord;
 
 uniform float time;
-uniform float amplitude;
 uniform vec3 baseColor;
 uniform float density;
 uniform float opacity;
 uniform float speed;
 uniform float spinSpeed;
 uniform vec2 drift;
-uniform float intensity;
 uniform float birth;
 uniform float death;
 
+uniform vec2 ripplePositions[12]; // x, y
+uniform vec3 rippleData[12];      // startTime, amplitude, frequency
+
 const float UVSCALE = 10.0;
+const float propogationSpeed = 0.75;
 
-vec2 ripple() {
-  vec2 normUV = (uvCoord - vec2(0.5)) * 2.0 * UVSCALE;
+vec2 ripple(vec2 center, float started, float amp, float freq) {
+  vec2 normUV = ((uvCoord - vec2(0.5)) * 2.0) * UVSCALE + center;
 
-  float distortion = (1.0 + cos(length(normUV) * intensity - time * speed));
-  float mainRipple = distortion * pow(amplitude, 3.0);
+  float distortion = (1.0 + cos(length(normUV) * freq - time * speed));
+  float mainRipple = distortion * pow(amp, 3.0);
 
-  return normUV * mainRipple;
+  float distance = length(normUV);
+  float sinceStart = time - started;
+  
+  // float freqSpeedCoef = smoothstep(350.0, 50.0, freq) + 1.0;
+
+  float attenuation = smoothstep(propogationSpeed * sinceStart, 0.0, distance);
+  // lower effect in center?
+  
+  return normUV * mainRipple * attenuation;
 }
 
 void main() {
   vec2 coords = (uvCoord - vec2(0.5)) * UVSCALE;
-  vec2 theRipple = ripple();
+  vec2 theRipples = vec2(0.0);
+  for (int i = 0; i < 12; i++) {
+    theRipples += ripple(
+      ripplePositions[i],
+      rippleData[i].x,
+      rippleData[i].y,
+      rippleData[i].z
+    );
+  }
+  
 
   // birth alpha
   float alpha = birth / length(coords);
@@ -37,13 +56,13 @@ void main() {
   // Highlight
   float highlight = dot(
     vec3(cos(spinSpeed * time), sin(spinSpeed * time), 0.0),
-    normalize(vec3(theRipple, 1.0))
+    normalize(vec3(theRipples, 1.0))
   );
 
   highlight = smoothstep(0.02, 0.2, abs(highlight)) * 0.6;
 
   // Add ripple
-  coords = coords + theRipple;
+  coords = coords + theRipples;
 
   // Spin
   float spin = spinSpeed * time;
@@ -56,12 +75,19 @@ void main() {
   coords += drift * time;
 
   // Grid
-  float value = smoothstep(0.75, 0.85, abs(mod(coords.x, 1.0 / density) * density - 0.5) * 2.0);
-  value      -= smoothstep(0.75, 0.85, abs(mod(coords.y, 1.0 / density) * density - 0.5) * 2.0) * 0.75;
+  float grid = smoothstep(0.75, 0.85, abs(mod(coords.x, 1.0 / density) * density - 0.5) * 2.0);
+  grid      -= smoothstep(0.75, 0.85, abs(mod(coords.y, 1.0 / density) * density - 0.5) * 2.0) * 0.75;
 
   // Trough shadow
-  float shadow = length(theRipple) * 0.75;
+  float shadow = length(theRipples) * 0.75;
 
-  //gl_FragColor = vec4(vec3(length(theRipple)), 1.0);
-  gl_FragColor = vec4(baseColor + vec3(value * opacity) - vec3(shadow) + vec3(highlight), alpha);
+  
+  gl_FragColor.xyz = (
+    baseColor
+    + vec3(grid * opacity)
+    + vec3(highlight)
+    - vec3(shadow)
+  );
+
+  gl_FragColor.w = alpha;
 }
--- echoes/background/gridstort/main.coffee
+++ echoes/background/gridstort/main.coffee
@@ -1,30 +1,36 @@
 module.exports = class Gridstort extends Echotron.Echo
   uniformAttrs:
     time:      'f'
-    amplitude: 'f'
     baseColor: 'c'
     density:   'f'
     opacity:   'f'
     speed:     'f'
     spinSpeed: 'f'
     drift:     'v2'
-    intensity: 'f'
     birth:     'f'
     death:     'f'
+
+    ripplePositions: 'v2v'
+    rippleData:      'v3v'
 
 
   initialize: ->
     @time         = THREE.Math.randFloat 0, 10
-    @amplitude    = 0
-    @amplitudeDir = 1
     @density      = THREE.Math.randFloat 10, 30
-    @opacityMax   = THREE.Math.randFloat 0.05, 0.2
+    @opacityMax   = THREE.Math.randFloat 0.05, 0.1
     @opacity      = @opacityMax
-    @speed        = THREE.Math.randFloat 5, 20
+    @speed        = 20#THREE.Math.randFloat 5, 20
     @spinSpeed    = THREE.Math.randFloatSpread(45).degToRad
-    @intensity    = THREE.Math.randFloat 15, 75
     @birth        = 0
     @death        = 0
+
+    @ripplePositions =
+      for i in [1..12]
+        new THREE.Vector2
+
+    @rippleData =
+      for i in [1..12]
+        new THREE.Vector3
 
     @drift        = new THREE.Vector2(
       THREE.Math.randFloatSpread 0.3
@@ -52,32 +58,51 @@ module.exports = class Gridstort extends Echotron.Echo
 
     @rotation.x = THREE.Math.randFloat(-130, -210).degToRad
 
-  onBeat: ->
-    @opacity = @opacityMax
-
   onBar: ->
-    @amplitudeDir = 1
+    @ripple {
+      amplitude: 1.5
+      frequency: 30
+      x: 0
+      y: 0
+    }
+
+  onBeat: ->
+    @ripple {
+      amplitude: 1.1
+      frequency: 150
+      x: THREE.Math.randFloatSpread 1
+      y: THREE.Math.randFloatSpread 1
+    }      
+
+  ripple: (options) ->
+    start = performance.now() / 1000
+    @lastRippleIndex ?= 0
+    
+    @rippleData[@lastRippleIndex].set(
+      start
+      options.amplitude
+      options.frequency
+    )
+
+    @ripplePositions[@lastRippleIndex].set(
+      options.x
+      options.y
+    )
+
+    @lastRippleIndex++
+    @lastRippleIndex = 0 if @lastRippleIndex >= @ripplePositions.length
 
   update: (elapsed) ->
     @birth += elapsed
-    @time += elapsed
+    @time = performance.now() / 1000
 
     unless @active
       @death += elapsed
 
-    if @amplitudeDir is 1
-      @amplitude += elapsed * stage.song.bps * 4
-      if @amplitude > 1
-        @amplitude = 1
-        @amplitudeDir = -1
-
-    else
-      @amplitude -= (elapsed * stage.song.bps) / 4
-      @amplitude = 0 if @amplitude < 0
+    for ripple in @rippleData
+      ripple.y -= (elapsed * stage.song.bps) * 0.4
+      ripple.y = 0 if ripple.y < 0
     
-    @opacity -= @opacityMax * elapsed * stage.song.bps * 0.65
-    @opacity = 0.02 if @opacity < 0.02
-
     @rotation.z += elapsed / 4.0
 
   alive: ->
