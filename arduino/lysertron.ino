7d56dd05c965928b70acb743569447dc882ecbbc
Alex Wayne <alex@beautifulpixel.com>
2014-04-19 23:07:15 -0700

    This robot can dance! Poorly :(

--- arduino/lysertron.ino
+++ arduino/lysertron.ino
@@ -2,9 +2,21 @@
 
 Servo barServo;
 
-const int pinServo    = 9;
-const int pinLedGreen = 5;
-const int pinLedRed   = 6;
+struct Joint {
+  int pin;
+  float offset;
+  float dir;
+  Servo servo;
+};
+
+Joint FL1 = { 2,  15,  1 };
+Joint FL2 = { 3,  -5, -1 };
+Joint FR1 = { 4, -10, -1 };
+Joint FR2 = { 5,  15,  1 };
+Joint BR1 = { 6,   0,  1 };
+Joint BR2 = { 7, -15, -1 };
+Joint BL1 = { 8,   0, -1 };
+Joint BL2 = { 9, -10,  1 };
 
 const int EVT_SEGMENT  = 1;  // 00000001
 const int EVT_TATUM    = 2;  // 00000010
@@ -12,57 +24,103 @@ const int EVT_BEAT     = 4;  // 00000100
 const int EVT_BAR      = 8;  // 00001000
 const int EVT_SECTION  = 16; // 00010000
 
-const float dimTime = 0.25; // seconds it takes for the led to fade.
-
-float lightValueGreen = 1.0;
-float lightValueRed   = 1.0;
-
 long lastLoop = 0;
 
-bool beatServoOn = false;
-bool barServoOn  = false;
+float sinceBeat;
+float untilBeat;
+
+float sinceBar;
+float untilBar;
+
+float beatDir = 1;
+float barDir  = 1;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pinLedGreen, OUTPUT);
-  pinMode(pinLedRed,   OUTPUT);
-  barServo.attach(pinServo);
+  
+  jointInit(FL1);
+  jointInit(FL2);
+  jointInit(FR1);
+  jointInit(FR2);
+  jointInit(BR1);
+  jointInit(BR2);
+  jointInit(BL1);
+  jointInit(BL2);
 }
 
 void loop() {
   float thisLoop = millis();
-  float decreaseAmount = (float)(thisLoop - lastLoop) / (dimTime * 1000.0);
+  float elapsed = (thisLoop - lastLoop) / 1000;
+  
 
-  lightValueRed = lightValueRed - decreaseAmount;
-  if (lightValueRed < 0.0) lightValueRed = 0.0;
+  sinceBeat += elapsed;
+  untilBeat -= elapsed;
+  if (untilBeat < 0) untilBeat = 0;
 
-  lightValueGreen = lightValueGreen - decreaseAmount;
-  if (lightValueGreen < 0.0) lightValueGreen = 0.0;
+  sinceBar += elapsed;
+  untilBar -= elapsed;
+  if (untilBar < 0) untilBar = 0;
 
   while (Serial.available() > 0) {
+    // float serialFloat;
+
+    // // first value is beat
+    // serialFloat = Serial.parseFloat();
+    // if (serialFloat > 0) {
+    //   // startBeat(serialFloat);
+    //   jointMove(FL2, random(-10, 10));
+    // }
+
+    // // second is bar
+    // serialFloat = Serial.parseFloat();
+    // if (serialFloat > 0) {
+    //   // untilBar = serialFloat;
+    // }
+
     int serialVal = Serial.read();
 
     // Check presence of event bits with a binary and operation.
     if (serialVal & EVT_BEAT)  {
-      lightValueGreen = 1.0;
-      beatServoOn = !beatServoOn;
+      startBeat(0.2);
     }
 
     if (serialVal & EVT_BAR) {
-      lightValueRed = 1.0;
-      barServoOn = !barServoOn;
-      beatServoOn = barServoOn;
+      untilBar = 0.2;
+      sinceBar = 0;
+      barDir *= -1;
     }
-
   }
 
-  analogWrite(pinLedGreen, 255 * lightValueGreen);
-  analogWrite(pinLedRed,   255 * lightValueRed);
-
-  int barAngle  = barServoOn ? 55 : 125;
-  int beatAngle = beatServoOn ? -10 : 10;
-  barServo.write(barAngle + beatAngle);
-
+  float completion = sinceBeat / (sinceBeat + untilBeat);
+  jointMove(FL2, 60 - 5 * completion * beatDir);
+  jointMove(FR2, 60 - 5 * completion * beatDir);
+  jointMove(BL2, 60 - 5 * completion * beatDir);
+  jointMove(BR2, 60 - 5 * completion * beatDir);  
+  
+  completion = sinceBar / (sinceBar + untilBar);
+  jointMove(FL1, 30 - 10 * completion * barDir);
+  jointMove(FR1, 30 - 10 * completion * barDir);
+  jointMove(BL1, 30 - 10 * completion * barDir);
+  jointMove(BR1, 30 - 10 * completion * barDir);
+  
   lastLoop = thisLoop;
 }
 
+void startBeat(float duration) {
+  beatDir *= -1;
+  untilBeat = duration;
+  sinceBeat = 0;
+}
+
+void jointInit(Joint joint) {
+  joint.servo.attach(joint.pin);
+  jointMove(joint, 0);
+}
+
+void jointMove(Joint joint, float angle) {
+  joint.servo.write(
+    90 + joint.offset + (
+      angle * joint.dir
+    )
+  );
+}
--- js/arduino.coffee
+++ js/arduino.coffee
@@ -24,8 +24,10 @@ Lysertron.Arduino =
 
           chrome.serial.onReceive.addListener (info) ->
             uint8View = new Uint8Array(info.data)
-            value = String.fromCharCode(uint8View[0])
-            console.log 'Serial Data Received', value
+            result = ''
+            for i in [0...uint8View.length]
+              result = result + String.fromCharCode(uint8View[i])
+            console.log 'Serial Data Received', result
 
   # Callback with the path of the serial port with the Arduino.
   getArduinoSerialPath: (cb) ->
@@ -36,10 +38,18 @@ Lysertron.Arduino =
 
   sendByte: (byte) ->
     buffer = new ArrayBuffer(1)
-    uint8View = new Uint8Array(buffer)
-    uint8View[0] = byte
+    intBufferView = new Uint8Array(buffer)
+    intBufferView[0] = byte
     chrome.serial.send @connection.info.connectionId, buffer, ->
 
+  sendFloats: (floats) ->
+    buffer = new ArrayBuffer(floats.length * 4)
+    floatBufferView = new Float32Array(buffer)
+
+    for i in [0...floats.length]
+      floatBufferView[i] = floats[i]
+
+    chrome.serial.send @connection.info.connectionId, buffer, ->
 
   dispatchMusicEvent: (data) ->
     byte = 0
@@ -50,8 +60,13 @@ Lysertron.Arduino =
     byte = byte | @flags.bar     if data.bar
     byte = byte | @flags.section if data.section
 
+    durations = []
+    durations.push(if data.beat then data.beat.duration else -1)
+    durations.push(if data.bar  then data.bar.duration  else -1)
+
     # console.log 'bitmasked serial byte', byte.toString(2), byte
-    @sendByte byte
+    @sendByte(byte)
+    # @sendFloats(durations)
 
 # Find the arduino!
 Lysertron.Arduino.connect()
